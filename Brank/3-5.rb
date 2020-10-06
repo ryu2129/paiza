cecb306d6a20dc2decb56ad1a983b759552d92d2
Ryunosuke Abe <ryu212920@gmail.com>
2020-10-06 21:14:32 +0900

    start 4-6

--- Brank/3-5.rb
+++ Brank/3-5.rb
@@ -1,8 +1,7 @@
 #スペース区切りで2つの整数が入力されるので、その区間の整数を全て表示してください。
 
-n = gets.chomp.split(" ")
-s = gets.chomp.split("")
+nums = gets.chomp.split(' ')
 
-for i in (n[0].to_i - 1)..(n[1].to_i - 1)
-    print s[i]
+for i in nums[0]..nums[1]
+  puts i
 end
--- Brank/3-final.rb
+++ Brank/3-final.rb
@@ -1,2 +1,8 @@
 #スペース区切りの2つの整数と、文字列が入力されるので、2つの整数の範囲の部分文字列を出力してください。
 
+n = gets.chomp.split(" ")
+s = gets.chomp.split("")
+
+for i in (n[0].to_i - 1)..(n[1].to_i - 1)
+    print s[i]
+end
